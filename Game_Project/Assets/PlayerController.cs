5beef7153fe55aa7707e2c8d5f987fb3beb9e03d
STIL <stil3113@mail.ru>
2014-11-07 16:27:34 +0500

    Player can move now

--- Game_Project/Assets/PlayerController.cs
+++ Game_Project/Assets/PlayerController.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+	public float maxSpeed = 10f;
+	public int health = 3;
 
 	// Use this for initialization
 	void Start () {
@@ -9,7 +12,11 @@ public class PlayerController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+		float moveX = Input.GetAxis ("Horizontal");
+		float moveY = Input.GetAxis ("Vertical");
+		rigidbody2D.velocity = new Vector2 (moveX * maxSpeed, moveY * maxSpeed);
 	
 	}
 }
