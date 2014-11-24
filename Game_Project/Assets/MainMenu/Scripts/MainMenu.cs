eef851b5703eb135a6691d37359277bc1f2cd56e
STIL <stil3113@mail.ru>
2014-11-24 16:04:32 +0500

    Start implementation Menu

---
+++ Game_Project/Assets/MainMenu/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+	public Texture backgroundTexture;
+
+	void OnGUI()
+	{
+		GUI.DrawTexture (new Rect(0,0,Screen.width,Screen.height),backgroundTexture);
+		if (GUI.Button(new Rect(Screen.width * .45f, Screen.height * .5f, Screen.width * .5f, Screen.height * .05f),"Play Game"))
+		{
+			print ("Clicked Play Game");
+		}
+
+		if (GUI.Button(new Rect(Screen.width * .55f, Screen.height * .6f, Screen.width * .4f, Screen.height * .05f),"Options"))
+		{
+			print ("Clicked Options");
+		}
+	}
+}
