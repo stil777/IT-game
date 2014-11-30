5a68a31dc4421ebf8b116bd55e5935eb5d7364de
SLav <akaslavonic@gmail.com>
2014-11-30 20:20:38 +0500

    fdsf

---
+++ IT-game-master/Game_Project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+	public float xMargin = 0f; // Distance in the x axis the player can move before the the camera follows
+	public float yMargin = 0f; // Distance in the y axis the player can move before the the camera follows
+	public float xSmooth = 8f; // How smoothly the camera catches up with it's target movement in the x axis
+	public float ySmooth = 8f; // How smoothly the camera catches up with it's target movement in the y axis
+	public Vector2 maxXandY; // The maximum x and y coordinates the camera can have
+	public Vector2 minXandY; // The minimum x and y coordinates the camera can have
+	public GUIElement gui;
+
+	private GameObject player;
+	
+	void Awake ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	bool CheckXMargin ()
+	{
+		return Mathf.Abs(transform.position.x - player.transform.position.x) > xMargin;
+	}
+
+	bool CheckYMargin ()
+	{
+		return Mathf.Abs(transform.position.y - player.transform.position.y) > yMargin;
+	}
+
+	void FixedUpdate ()
+	{
+		TrackPlayer ();
+		gui.guiText.text = "Health: " + ((int)(player.GetComponent<PlayerController> ().health)).ToString () + "   St's Cards: " + ((int)(player.GetComponent<PlayerController> ().stsCards)).ToString ();
+	}
+
+	void TrackPlayer ()
+	{
+		float targetX = transform.position.x;
+		float targetY = transform.position.y;
+
+		if (CheckXMargin ()) targetX = Mathf.Lerp (transform.position.x, player.transform.position.x, xSmooth * Time.deltaTime);
+		if (CheckYMargin ()) targetY = Mathf.Lerp (transform.position.y, player.transform.position.y, ySmooth * Time.deltaTime);
+
+		targetX = Mathf.Clamp (targetX, minXandY.x, maxXandY.x);
+		targetY = Mathf.Clamp (targetY, minXandY.y, maxXandY.y);
+
+		transform.position = new Vector3 (targetX,targetY,transform.position.z);
+
+	}
+}
---
+++ IT-game-master/Game_Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	private Rigidbody2D myRigidbody;
+	private Animator anim;
+	private bool facingRight = false;
+	private GameObject Enemy;
+	private GameObject Player;
+	private float maxSpeed = 4f;
+	public int health = 3;
+	public int stsCards = 0;
+	private float lastHitTime = -3f;
+	private float repeatDamagePeriod = 3f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		myRigidbody = this.rigidbody2D;
+		anim = GetComponent<Animator> ();
+		Enemy = GameObject.FindGameObjectWithTag ("Enemy");
+		Player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float moveX = Input.GetAxisRaw ("Horizontal");
+		float moveY = Input.GetAxisRaw ("Vertical");
+		float posX = 0;
+		float posY = 0;
+		if (myRigidbody.velocity.x != posX) anim.SetFloat ("Speed", Mathf.Abs(moveX));
+		else if (myRigidbody.velocity.y != posY) anim.SetFloat ("Speed", Mathf.Abs(moveY));
+		myRigidbody.velocity = new Vector2 (moveX * maxSpeed, moveY * maxSpeed);
+
+		if (facingRight == true && moveX < 0)
+		{
+			Flip();
+		}
+		else if (facingRight == false && moveX > 0)
+		{
+			Flip();
+		}
+
+		//Taking damage
+		Vector2 hurtVector = Player.transform.position - Enemy.transform.position;
+		if (Time.time > lastHitTime + repeatDamagePeriod)
+		{
+			if (health > 0)
+			{
+				if (hurtVector.x * hurtVector.x + hurtVector.y * hurtVector.y < 1.5f)
+				{
+					health--;
+					lastHitTime = Time.time;
+					//print("Health - " + health + "; Time: " + Time.time);
+				}
+			}
+		}
+		//print (health);
+	}
+
+	void Flip ()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
