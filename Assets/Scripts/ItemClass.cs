1ab667b51b7deabbba92bead031795befbd2a1fa
SLav <akaslavonic@gmail.com>
2014-11-30 21:19:28 +0500

    some fix

---
+++ Assets/Scripts/ItemClass.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemClass : MonoBehaviour
+{
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			if (this.tag == "Health" && other.GetComponent<PlayerController>().health < other.GetComponent<PlayerController>().maxHealth)
+			{
+				other.GetComponent<PlayerController>().health++;
+				Destroy(transform.root.gameObject);
+			}
+			if (this.tag == "StCard")
+			{
+				other.GetComponent<PlayerController>().stsCards++;
+				Destroy(transform.root.gameObject);
+			}
+			if (this.tag == "Key")
+			{
+				other.GetComponent<PlayerController>().keys++;
+				Destroy(transform.root.gameObject);
+			}
+			if (this.tag == "HWater")
+			{
+				other.GetComponent<PlayerController>().holyWater++;
+				Destroy(transform.root.gameObject);
+			}
+			if (this.tag == "UnionCard")
+			{
+				other.GetComponent<PlayerController>().unionCard = true;
+				print ("You found Union Card");
+				other.GetComponent<PlayerController>().maxSpeed++;
+				Destroy(transform.root.gameObject);
+			}
+			if (this.tag == "LibCard")
+			{
+				other.GetComponent<PlayerController>().readCard = true;
+				print ("You found Read Card");
+				other.GetComponent<PlayerController>().maxHealth++;
+				Destroy(transform.root.gameObject);
+			}
+			if (this.tag == "Badge")
+			{
+				other.GetComponent<PlayerController>().policeBadge = true;
+				print ("You found Police Badge");
+				Destroy(transform.root.gameObject);
+			}
+			if (this.tag == "Diploma")
+				print ("You won a Game!");
+
+		}
+	}
+}
